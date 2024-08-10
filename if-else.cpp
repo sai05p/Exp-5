bc7c670bd4598820393805cca0672fed4826ad08
sai05p <saipunadikar19@gmail.com>
2024-08-10 20:01:48 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,22 @@
+//Riddhi Musale
+//Prn:23070123108
+//Exp 5 If else statement 
+#include <iostream>
+using namespace std;
+int main(){
+    int a=34,b=44;
+    if(a>b){
+        cout<<"a is greater than b";
+
+    }
+    else if(a<b){
+        cout<<"b is greater than a ";
+
+    }
+    else{
+        cout<<"both are equal";
+
+    }
+    return 0;
+}
+
